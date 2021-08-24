c805feb70dbdb7f6b47d97c8b1885aa85540cd04
Mark Ferry <mark@markferry.net>
2021-08-24 17:18:04 +0100

    Lower 'Waiting for metadata' log to DEBUG level

--- server/streamreader/airplay_stream.cpp
+++ server/streamreader/airplay_stream.cpp
@@ -252,7 +252,7 @@ void AirplayStream::pipeReadLine()
             {
                 // For some reason, EOF is returned until the first metadata is written to the pipe.
                 // If shairport-sync has not finished setting up the pipe, bad file descriptor is returned.
-                LOG(INFO, LOG_TAG) << "Waiting for metadata, retrying in 2500ms\n";
+                LOG(DEBUG, LOG_TAG) << "Waiting for metadata, retrying in 2500ms\n";
                 wait(pipe_open_timer_, 2500ms, [this] { pipeReadLine(); });
             }
             else
